087a0dd23441cad393a3d89324df4af1d3e2cff9
Ruslan Kurbanov <wkurbanov@gmail.com>
2017-01-23 21:21:25 +0300

    Minify js/custom_theme.js, js/yandex_map.js, replace link bootstrap.js with bootstrap.min.js

--- js/yandex_map.js
+++ js/yandex_map.js
@@ -1,14 +1,1 @@
-ymaps.ready(init);
-function init() {
-    var map = new ymaps.Map("map", {
-        center: [55.740684, 37.618174],
-        zoom: 15
-    });
-    map.behaviors.disable('scrollZoom');
-    placemark = new ymaps.Placemark([55.740684, 37.618174], {
-        hintContent: 'Твой Дом',
-        balloonContentHeader: 'Твой Дом',
-        balloonContentBody: 'Москва, Старомонетный переулок, 10'
-    });
-    map.geoObjects.add(placemark);
-}
+function init(){var a=new ymaps.Map("map",{center:[55.740684,37.618174],zoom:15});a.behaviors.disable("scrollZoom"),placemark=new ymaps.Placemark([55.740684,37.618174],{hintContent:"Твой Дом",balloonContentHeader:"Твой Дом",balloonContentBody:"Москва, Старомонетный переулок, 10"}),a.geoObjects.add(placemark)}ymaps.ready(init);
